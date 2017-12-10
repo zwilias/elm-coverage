b58c30c3efad383fe985d1013fdbf61d0fd9f4f7
ilias <mail@ilias.xyz>
2017-12-10 16:39:37 +0100

    Start setting up tests

---
+++ tests/runner.js
@@ -0,0 +1,23 @@
+var spawn = require("cross-spawn"),
+    chai = require("chai"),
+    assert = chai.assert,
+    shell = require("shelljs");
+
+var elmCoverage = require.resolve("../bin/elm-coverage");
+
+describe("The runner", function() {
+    it("prints the usage instructions when running with `--help`", function(done) {
+        var process = spawn.spawn(elmCoverage, ["--help"]);
+        var output = "";
+
+        process.stdout.on("data", function(data) {
+            output += data;
+        });
+
+        process.on("exit", function(exitCode) {
+            assert.equal(exitCode, 0, "Expected to exit with 0 exitcode");
+            assert.notEqual(output, "", "Expected to have some output");
+            done();
+        });
+    });
+});
