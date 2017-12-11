8d13f7c97c2e7ca443607db3ca400c32ddfff37e
ilias <mail@ilias.xyz>
2017-12-12 00:53:00 +0100

    Bump

--- install.js
+++ install.js
@@ -1,38 +1,19 @@
-var binstall = require("binstall");
+var binwrap = require("binwrap");
 var path = require("path");
-var fs = require("fs");
-var packageInfo = require(path.join(__dirname, "package.json"));
 
-// 'arm', 'ia32', or 'x64'.
-var arch = process.arch;
-// 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
-var operatingSystem = process.platform;
+var binVersion = "0.0.4";
 
-var filename = operatingSystem + "-" + arch + ".tar.gz";
-var version = "0.0.3";
-var url =
-    "https://dl.bintray.com/zwilias/elm-coverage/elm-instrument/" +
-    version +
-    "/" +
-    filename;
+var root =
+    "https://github.com/zwilias/elm-instrument/releases/download/" +
+    binVersion +
+    "/";
 
-var binariesDir = path.join(__dirname, "bin");
-var binaryExtension = process.platform === "win32" ? ".exe" : "";
-
-var executablePaths = Object.keys(packageInfo.bin).map(function(executable) {
-    return path.join(binariesDir, executable + binaryExtension);
-});
-
-binstall(
-    url,
-    { path: binariesDir, strip: 0 },
-    { verbose: true, verify: executablePaths }
-).then(
-    function(successMessage) {
-        console.log(successMessage);
-    },
-    function(errorMessage) {
-        console.error(errorMessage);
-        process.exit(1);
+module.exports = binwrap({
+    binaries: ["elm-instrument"],
+    urls: {
+        "darwin-x64": root + "osx-x64.tar.gz",
+        "linux-x64": root + "linux-x64.tar.gz",
+        "win32-x64": root + "windows-x64.tar.gz",
+        "win32-ia32": root + "windows-ia32.tar.gz"
     }
-);
+});
