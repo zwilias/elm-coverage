4565aab9f993dd4312ca1858406c7aef38309aaa
ilias <mail@ilias.xyz>
2017-12-26 11:34:25 +0100

    Add some more tests

--- tests/runner.js
+++ tests/runner.js
@@ -1,24 +1,32 @@
 var spawn = require("cross-spawn"),
     chai = require("chai"),
+    Promise = require("bluebird"),
     assert = chai.assert,
+    expect = chai.expect,
     shell = require("shelljs"),
-    path = require("path");
+    path = require("path"),
+    fs = require("fs-extra"),
+    chaiMatchPattern = require("chai-match-pattern");
+
+chai.use(require("chai-json-schema-ajv"));
+chai.use(chaiMatchPattern);
+var _ = chaiMatchPattern.getLodashModule();
 
 var elmCoverage = require.resolve("../bin/elm-coverage");
 
-describe("Sanity test", function() {
-    it("prints the usage instructions when running with `--help`", function(done) {
+describe("Sanity test", () => {
+    it("prints the usage instructions when running with `--help`", done => {
         var process = spawn.spawn(elmCoverage, ["--help"]);
         var output = "";
 
-        process.stderr.on("data", function(data) {
+        process.stderr.on("data", data => {
             console.error(data.toString());
         });
-        process.stdout.on("data", function(data) {
+        process.stdout.on("data", data => {
             output += data;
         });
 
-        process.on("exit", function(exitCode) {
+        process.on("exit", exitCode => {
             assert.equal(exitCode, 0, "Expected to exit with 0 exitcode");
             assert.notEqual(output, "", "Expected to have some output");
             done();
@@ -28,18 +36,72 @@ describe("Sanity test", function() {
 
 describe("E2E tests", function() {
     this.timeout(Infinity);
-    it("Should run succesfully", function(done) {
+    it("Should run succesfully", done => {
         var process = spawn.spawn(elmCoverage, {
             cwd: path.join("tests", "data", "simple")
         });
 
-        process.stderr.on("data", function(data) {
+        process.stderr.on("data", data => {
             console.error(data.toString());
         });
 
-        process.on("exit", function(exitCode) {
+        process.on("exit", exitCode => {
             assert.equal(exitCode, 0, "Expected to finish succesfully");
             done();
         });
     });
+
+    it("Should generate schema-validated JSON", () =>
+        Promise.all([
+            fs.readJSON(require.resolve("../docs/elm-coverage.json")),
+            generateJSON()
+        ]).spread((json, schema) => {
+            expect(json).to.be.jsonSchema(schema);
+        }));
+
+    it("Should generate JSON that matches the pregenerated one, modulus runcount", () =>
+        Promise.all([
+            generateJSON(),
+            fs.readJSON(require.resolve("./data/simple/expected.json"))
+        ]).spread((actual, expectedJSON) => {
+            var expected = expectedJSON;
+            expected.coverageData = _.mapValues(
+                expectedJSON.coverageData,
+                moduleData =>
+                    _.map(moduleData, coverage =>
+                        Object.assign({}, coverage, {
+                            count: _.isInteger
+                        })
+                    )
+            );
+
+            expect(actual).to.matchPattern(expected);
+        }));
 });
+
+function generateJSON() {
+    return new Promise((resolve, reject) => {
+        var process = spawn.spawn(
+            elmCoverage,
+            ["generate", "--report", "json"],
+            {
+                cwd: path.join("tests", "data", "simple")
+            }
+        );
+
+        var output = "";
+
+        process.stdout.on("data", data => {
+            output += data;
+        });
+
+        process.on("exit", exitCode => {
+            assert.equal(exitCode, 0, "Expected to finish succesfully");
+            if (exitCode === 0) {
+                resolve(output);
+            } else {
+                reject(new Error("Expected to finish successfully"));
+            }
+        });
+    }).then(json => JSON.parse(json));
+}
